3ed5c44d2e5fe484b5da359ff695a546956df40c
Jake Teton-Landis <just.1.jake@gmail.com>
2022-09-23 11:53:54 -0700

    update typings

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,26 @@
-export default function scraper(
-  url: string
-): Promise<{
-  url: string;
-  name: string;
-  image: string;
-  description: string;
-  cookTime: string;
-  prepTime: string;
-  totalTime: string;
-  recipeYield: string;
-  recipeIngredients: string[];
-  recipeInstructions: string[];
-  recipeCategories: string[];
-  recipeCuisines: string[];
-  recipeTypes: string[];
-  keywords: string[];
-}>;
+interface ScrapedRecipeData {
+  url?: string;
+  name?: string;
+  image?: string;
+  description?: string;
+  cookTime?: string;
+  prepTime?: string;
+  totalTime?: string;
+  recipeYield?: string;
+  recipeIngredients?: string[];
+  recipeInstructions?: string[];
+  recipeCategories?: string[];
+  recipeCuisines?: string[];
+  recipeTypes?: string[];
+  keywords?: string[];
+}
+
+export function parse(
+  html: string,
+  options?: {
+    url?: string;
+    printToConsole?: boolean;
+  }
+): ScrapedRecipeData;
+
+export default function scraper(url: string): Promise<ScrapedRecipeData>;
