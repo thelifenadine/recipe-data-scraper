e14aaddf5fde4b308121bc001e4fc3c2e8e181ec
Matthew Carlson <matthewfcarlson+github@gmail.com>
2023-01-03 00:16:03 -0600

    Fix issue #31- handle types that are arrays

--- src/scrapers/JsonLdScraper.js
+++ src/scrapers/JsonLdScraper.js
@@ -1,6 +1,19 @@
 import logger from '../utils/logger';
 import Scraper from './Scraper';
 
+function hasRecipeType(meta) {
+  // Check if @type exists
+  if (meta['@type'] == undefined) return false;
+  let type = meta["@type"]
+  // If it's recipe, we're golden
+  if (type === 'Recipe') return true;
+  // If it's an array, see if it's in there
+  if (Array.isArray(type)) {
+    if (type.indexOf("Recipe") != -1) return true;
+  }
+  return false;
+}
+
 class JsonLdScraper extends Scraper {
   constructor(chtml) {
     super(chtml);
@@ -31,20 +44,20 @@ class JsonLdScraper extends Scraper {
       logger('Error: No JSON-LD valid script tags present on page');
       return;
     }
-
+    
     this.meta = json.length > 1 ? json : json[0];
   }
 
   findRecipeItem() {
-    if (this.meta['@type'] === 'Recipe') {
-      // nytimes, food.com, bonappetite, ohsheglows, simplyrecipes
+    // nytimes, food.com, bonappetite, ohsheglows, simplyrecipes
+    if (hasRecipeType(this.meta)) {
       this.recipeItem = this.meta;
       return;
     }
     // @graph: king arthur, 12tomatoes, sallysbaking, cookie&kate
     // other: martha stewart, foodnetwork, eatingwell, allrecipes, myrecipes, seriouseats, skinnytaste
     const graphLevel = this.meta['@graph'] || this.meta;
-    this.recipeItem = Object.values(graphLevel).find(item => (item['@type'] === 'Recipe'));
+    this.recipeItem = Object.values(graphLevel).find(item => (hasRecipeType(item)));
   }
 }
 
